e663b91f80486b71f83f57cc644668a89874eb55
themaxermister <subscribingalerts@gmail.com>
2020-06-10 22:49:34 +0800

    Added .gitignore

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
+for dirname, _, filenames in os.walk('/data'): #'/kaggle/input'
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
 # You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session`
